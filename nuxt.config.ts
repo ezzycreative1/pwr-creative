615c09ccb5a59f22ba03acb28d3b050a5c2206ef
robertus adi setyadharma <adi.setyadharma83@gmail.com>
2025-02-24 10:59:39 +0800

    update config awal

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   compatibilityDate: '2024-11-01',
+//   devtools: { enabled: true }
+// })
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  //target: 'static', // Untuk deploy di Netlify
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'PWR Creative - Your Creative Partner',
+      meta: [
+        { name: 'description', content: 'PWR Creative adalah studio kreatif yang menyediakan jasa desain, branding, dan pengembangan web.' }
+      ],
+    },
+  },
+});
